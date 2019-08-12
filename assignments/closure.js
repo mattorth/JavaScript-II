5751e5ee06ba60f037e126ee8830a69966a85c8a
Matthew John Orth <matthewjohnorth@Matthews-MacBook-Pro-72.local>
2019-08-11 18:12:12 -0700

    closures done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+num1 = 3;
+function multiplyNums(num2) {
+  return num1 * num2;
+}
+console.log(multiplyNums(4));
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,8 +14,18 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count = count + 1;
+    return count;
+  }
 };
 // Example usage: const newCounter = counter();
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
+
 // newCounter(); // 1
 // newCounter(); // 2
 
